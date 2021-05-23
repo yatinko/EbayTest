46b75fbc9f2ed2ff4cddcb9b6a8fbde312249840
yatinko <32841505+yatinko@users.noreply.github.com>
2021-05-24 01:18:16 +0530

    Completed 3 scenarios

--- src/test/java/StepDefinitions/HomepageSteps.java
+++ src/test/java/StepDefinitions/HomepageSteps.java
@@ -3,9 +3,7 @@
  */
 package StepDefinitions;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 
@@ -25,10 +23,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class HomepageSteps {
 	WebDriver driver;
 	Homepage_PF home;
+	String searchItemText, searchCategoryText;
 	
 	@Before
 	public void config() {
-//		Initializing web driver
+		// Initializing web driver
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 	}
@@ -39,64 +38,85 @@ public class HomepageSteps {
 	}
 	
 	@Given("User is already on HomePage")
-	public void user_is_already_on_home_page() {		
-//	    Navigating to the homepage
+	public void user_is_already_on_home_page() {
+		System.out.println("==================================================================================================================================================================");
+		
+		// Navigating to the homepage
+		driver.manage().window().maximize();
 		driver.get("https://www.ebay.com/");
-		System.out.println("========================================");
 	}
 
 	@Given("User is on HomePage")
 	public void user_is_on_home_page() {
-//		Initializing home object
+		// Initializing home object
 		home = new Homepage_PF(driver);
 		
-//		Getting navbar home component's text
+		// Getting navbar home component's text
 		String navHomeSpanString = home.getHomeNavText();
 		System.out.println(navHomeSpanString);
 	    
-//	   	Check if the home component is active
+		// Check if the home component is active
 	   	Assert.assertEquals(navHomeSpanString, "Home");
 	   	System.out.println("User is on homepage");
 	}
 
 	@Then("check the title of the page")
 	public void check_the_title_of_the_page() {
-//		Getting the page title
+		// Getting the page title
 		String title = driver.getTitle();
 		
-//		Checking if the title is correct
+		// Checking if the title is correct
 	    Assert.assertEquals("Electronics, Cars, Fashion, Collectibles & More | eBay", title);
 	    System.out.println("Title matched");
 	}
 
-	@When("When user enters <validSearchItem> & <validSearchCategory> in searchbar")
-	public void user_enters_valid_data_in_searchbar(String validSearchItem, String validSearchCategory) {
-	    
-		home.enterSearchText(validSearchItem);
-		home.selectSearchCategory(validSearchCategory);
+	@When("user enters {string} and {string} in searchbar")
+	public void user_enters_searchItem_and_searchCategory_in_searchbar(String searchItem, String searchCategory) {
+		// Saving the search items in the local variables
+		searchItemText = searchItem;
+		searchCategoryText = "Selected category\n" + searchCategory;
+		
+		// Entering the data in the searchbar
+		home.enterSearchText(searchItem);
+		home.selectSearchCategory(searchCategory);
 	}
 
 	@And("clicks the Search button")
 	public void clicks_the_search_button() {
+		// Clicking on the search button
 		home.searchProduct();
 	}
 
 	@Then("display relevant products")
 	public void display_relevant_products() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@When("user invalid data in searchbar")
-	public void user_invalid_data_in_searchbar() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		// Retrieving total number of products found
+		int totalProducts = home.checkTotalProducts();
+		
+		// If the searched product is found, continue
+		if(totalProducts > 0) {
+			// Verifying if results displayed is of the product searched
+			String searchResultText = home.checkIfProductIsCorrect();
+			String searchCategory = home.checkIfProductCategoryIsCorrect();
+		   
+			Assert.assertEquals(searchResultText , searchItemText);
+			Assert.assertEquals(searchCategory , searchCategoryText);
+		}
 	}
 
 	@Then("display appropriate message")
 	public void display_appropriate_message() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		// Retrieving total number of products found
+		int totalProducts = home.checkTotalProducts();
+		
+		// If the searched product is found, continue
+		if(totalProducts == 0) {
+			// Verifying if results displayed is of the product searched
+			String searchResultText = home.checkIfProductIsCorrect();
+			String warning = home.checkNoProductFoundWarning();
+			   
+			Assert.assertEquals(searchResultText , searchItemText);
+			Assert.assertEquals(warning, "No exact matches found");
+		}
 	}
 
 	@When("user hovers cursor on navbar elements")
